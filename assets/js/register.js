6b065f3f4e1f8707c3c12e0fd62ca44089877d8d
ZACKYYXXX4 <amimirkambing@gmail.com>
2025-09-11 20:54:49 +0700

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -17,7 +17,6 @@ import {
 const firebaseConfig = {
   apiKey: "AIzaSyB35rgDqqlPUsPf0UCy_IK-NbfvsPpz-4c",
   authDomain: "plant-1942d.firebaseapp.com",
-  databaseURL: "https://plant-1942d-default-rtdb.firebaseio.com",
   projectId: "plant-1942d",
   storageBucket: "plant-1942d.appspot.com",
   messagingSenderId: "331323609056",
@@ -96,3 +95,4 @@ document.getElementById("facebookLogin").addEventListener("click", async () => {
     alert(err.message);
   }
 });
+
